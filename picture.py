da89c1e9e1a4cfe802f29d3eb71c9b39680d02d0
CieloMV <cmezav@unsa.edu.pe>
2023-05-29 18:03:46 -0500

    Función up para concatenar imagenes, concatener p a self para que aparezca abajo

--- picture.py
+++ picture.py
@@ -39,7 +39,8 @@ class Picture:
     return Picture(join)
 
   def up(self, p):
-    return Picture(None)
+     image = self.img + p.img
+     return Picture(image)
 
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
