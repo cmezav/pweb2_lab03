fb2b4fcdb58cac0bc2290c9688a5e758b876b9ad
CieloMV <cmezav@unsa.edu.pe>
2023-05-30 12:12:04 -0500

    Prueba: Se creó un cuadrado negativo para que se concatene con uno normal 4 veces sólo usando join

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+
+squareN = square.negative()
+prueba = square.join(squareN.join(square.join(squareN.join(square.join(squareN.join(square.join(squareN)))))))
+
+draw(prueba)
