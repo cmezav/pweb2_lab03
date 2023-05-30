1b39a146cb51eb50677fe7bbe9ea8368dff37354
CieloMV <cmezav@unsa.edu.pe>
2023-05-29 23:23:04 -0500

    Se crearon dos knight con color negativo, uno de ellos invertido con verticalMirror

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,6 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+
 knightN = knight.negative()
 line1 = knight.join(knightN)
 
-draw(line1)
+knightN2 = knightN.verticalMirror()
+line2 = knightN2.join(knight.verticalMirror())
+
+final = line1.up(line2)
+
+draw(final)
