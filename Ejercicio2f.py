110762e2be7a8e89d6e515c631718c0fd136e63e
CieloMV <cmezav@unsa.edu.pe>
2023-05-30 21:28:03 -0500

    Prueba empleando horizontalRepeat y up para imprimir dos lineas

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+line1 = square.join(square.negative()).horizontalRepeat(4)
+line2 = square.negative().join(square).horizontalRepeat(4)
+
+final = line1.up(line2)
+
+draw(final)
+
