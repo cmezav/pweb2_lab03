c96804eef2bccecdc778d43b66241c3a6c964fba
CieloMV <cmezav@unsa.edu.pe>
2023-05-31 03:24:17 -0500

    Se une image2 con su negativo y se intercalan horizontalmente 4 veces

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -3,7 +3,6 @@ from chessPictures import *
 
 image = Picture(square.negative().img + square.img)
 image2 = image.verticalRepeat(2)
-
-draw(image2)
+draw(image2.join(image2.negative()).horizontalRepeat(4))
 
 
